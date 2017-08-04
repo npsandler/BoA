78f9da0bb1f56d4cf4a12fbf2ea90fc2bdd843bd
Nathaniel Sandler <npsandler@gmail.com>
2017-08-04 14:55:04 -0400

    set up react

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+let webpack = require('webpack')
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/app.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ]
+
+};
