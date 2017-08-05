77e8e153de58edd884cae41f954ea8904f80e16f
Nathaniel Sandler <npsandler@gmail.com>
2017-08-05 14:46:42 -0400

    initial page renedring

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ let webpack = require('webpack')
 
 module.exports = {
   context: __dirname,
-  entry: "frontend/BoA.jsx",
+  entry: "./frontend/BoA.jsx",
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
